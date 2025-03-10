c5b373c786a2d793c9225cb2d6da270258159901
Aaron Parandian <aparandian@apple.com>
2025-03-10 10:47:54 -0700

    01. initial random forest implementation

---
+++ random_forest_classification.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+# Load data
+iris = load_iris()
+df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+df["target"] = iris.target
+
+# Check for missing values
+print("Original dataset shape: ", df.shape)
+print("Number of missing or null values: ", df.isnull().sum())
+df = df.dropna(how="any")
+print("Dataset shape after removing missing values: ", df.shape)
+
+# Dataset statistics
+print("Dataset statistics: ")
+print(df.describe())
+
+# Preview dataset
+print("Preview dataset: ")
+print(df.head())
+
+# Feature matrix and target vector
+feature_names = [
+    "sepal length (cm)",
+    "sepal width (cm)",
+    "petal length (cm)",
+    "petal width (cm)",
+]
+target_name = "target"
+X = df.loc[:, feature_names].values
+y = df.loc[:, target_name].values
+
+print("Feature matrix shape: ", X.shape)
+print("Target vector shape: ", y.shape)
